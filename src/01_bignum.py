b3e8e927d109963c7559c04fdd9f8d7018da0206
newbi462 <isthisemailfree@gmail.com>
2020-03-31 10:54:08 -0400

    bignum -- Print some big numbers

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,7 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
+# JS wont do that, it truncates large numbers that is why you have all those numb to string whatnot puzels
+
 # YOUR CODE HERE
+print(2**65536)
