7d9a40b08270a14e4715937797019c7f96d7ce23
newbi462 <isthisemailfree@gmail.com>
2020-04-03 12:41:50 -0400

    file_io -- Read and write from files

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -3,6 +3,11 @@ Python makes performing file I/O simple. Take a look
 at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+
+with open("foo.txt", "r") as file:
+    read_data = file.read()
+        # print(read_data)
+    file.closed
 """
 
 # Open up the "foo.txt" file (which already exists) for reading
@@ -10,6 +15,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+import os
+print(os.getcwd())
+
+with open("src/foo.txt", "r") as file:
+    read_data = file.read()
+    print(read_data)
+file.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +29,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open("src/bar.txt", "w") as bar_file:
+    for i in range(3):
+        bar_file.write("This is line %d\r\n" % (i+1))
+bar_file.closed
+
+with open("src/bar.txt", "r") as bar_file:
+    read_data2 = bar_file.read()
+    print(read_data2)
+bar_file.closed
+# watch the white space but follow the errors
