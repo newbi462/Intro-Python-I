71e9bbb3bd5dff36476a6c2b8227cb178f436704
newbi462 <isthisemailfree@gmail.com>
2020-04-04 11:52:48 -0400

    cal -- Experiment with module imports and implement a text-based calendar

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,31 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+date_input = input("Enter a month: ")
+# print(date_input)
+
+# would be more simple to add input 2 to prevent errors... 
+
+# https://www.geeksforgeeks.org/python-string-isnumeric-application/
+if date_input.isnumeric():
+    if int(date_input) < 13:
+        date_input = int(date_input)
+    else:
+        date_input = datetime.today().month
+else:
+    date_input = datetime.today().month
+
+print(calendar.Calendar().monthdatescalendar(2020, date_input))
