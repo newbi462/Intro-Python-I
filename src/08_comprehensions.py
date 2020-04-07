3246c0418eeff0c3dcc20c496f4b0be526fc72f8
newbi462 <isthisemailfree@gmail.com>
2020-04-07 17:40:19 -0400

    What do you need between the square brackets to make it work?

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -36,7 +36,7 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [*x]
 # even as in "2" or as in [0]?
 
 print(y)
